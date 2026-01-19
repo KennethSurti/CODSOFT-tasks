1ba75a34febc8c2750f91123039c3d941ce435e2
KennethSurti <surtikenneth@gmail.com>
2026-01-19 10:56:47 +0530

    Theme and UI update

--- passgen.py
+++ passgen.py
@@ -11,3 +11,26 @@ import string
 from tkinter import messagebox
 
 #checking the libraries compatibility
+#Theme
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+app = ctk.CTk()
+app.title("Password Generator")
+app.geometry("500x420")
+app.resizable(False, False)
+
+# UI Layout
+ctk.CTkLabel(
+    app,
+    text="🔐 Password Generator",
+    font=("Segoe UI", 24, "bold")
+).pack(pady=15)
+
+length_entry = ctk.CTkEntry(
+    app,
+    width=300,
+    placeholder_text="Enter password length"
+)
+length_entry.pack(pady=10)
+
